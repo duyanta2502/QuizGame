5748be0152d3e7800f6c660b446e09c268c474b0
hnam2210 <namle.22102000@gmail.com>
2024-12-03 20:05:07 +0700

    init project

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken('my-app-token')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+    
+        $user = User::create([
+            'name' => $request->email,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+    
+    
+        return response()->json([
+            'message' => 'User registered successfully.',
+            'user' => $user,
+        ], 201);
+    }
+
+    public function logout(Request $request)
+    {
+		$request->user()->tokens()->delete();
+
+		$request->user()->currentAccessToken()->delete();
+
+		return response()->json(['message' => 'You have been successfully logged out.'], 200);
+    }
+}
